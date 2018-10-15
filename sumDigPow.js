fd5d264cdf6d604925c0604d47cdc6c11f58dd75
dbushey <polendeuva@gmail.com>
2018-10-15 17:03:39 -0400

    Solved a codewars kata sumDigPow

--- sumDigPow.js
+++ sumDigPow.js
@@ -1,1 +1,22 @@
+function sumDigPow(a, b) {
+  let finalArray = []
 
+  for(let i = a; i <= b; i++){
+    let digits = i.toString().length
+    // handle case of single digit
+    if (digits === 1){
+       finalArray.push(i)
+    } else {
+      let arrStr = i.toString().split("")
+      let arrNum = arrStr.map(Number)
+      let sum = 0
+      for(let k = 0; k < arrNum.length; k++){
+        sum += Math.pow(arrNum[k], k+1)
+      }
+        if(sum === i){
+          finalArray.push(sum)
+        }
+      }
+    }
+    return finalArray
+  }
